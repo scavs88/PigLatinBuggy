260abca9c1d7260c446621393552f664c224a887
sean <spcavanaugh88@gmail.com>
2021-04-29 09:01:31 -0400

    commit

---
+++ PigLatin/IsVowel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigLatin
+{
+    class IsVowel
+    {
+        private string _vowel;
+        
+
+    }
+}
--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -5,44 +5,51 @@ namespace PigLatin
 {
     public class Program
     {
+
+        //overall, user input is being stored and passed around to too many different variables.  Input should be stored in one value and used from that value 
+        //, processes should be in some sort of order in terms of where information is flowing
         static void Main(string[] args)
         {
-            string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
-
+            Console.WriteLine("Please input a word or sentence to translate to pig Latin");
+            string userInput = Console.ReadLine().ToLower().Trim();
             string translation = ToPigLatin(userInput);
-            Console.WriteLine(translation);
+            Console.WriteLine(translation); // not translating
         }
 
-        public static string GetInput(string prompt)
-        {
-            Console.WriteLine(prompt);
-            string input = Console.ReadLine().ToLower().Trim();
-            return input;
-        }
+        //public static string GetInput(string prompt)
+        //{
+        //    Console.WriteLine(prompt);                   <------------Easier and more clear write in main
+        //    string input = Console.ReadLine().ToLower().Trim();
+        //    return input;
+        //}
 
-        public static bool IsVowel(char c)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            
-            return c.ToString() == vowels.ToString();
-        }
+        
 
         public static string ToPigLatin(string word)
         {
-            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' }; //works, seems over complicated
             word = word.ToLower();
             foreach(char c in specialChars)
             {
-                foreach(char w in word)
+
+
+                if (word.Contains(c)) //shortened this to remove redundant foreach loop
                 {
-                    if (w == c)
-                    {
                         Console.WriteLine("That word has special characters, we will return it as is");
                         return word;
-                    }
                 }
                 
+                
             }
+
+
+            static bool IsVowel(char c)
+            {
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+                return c.ToString() == vowels.ToString();
+            }
+
 
             bool noVowels = true;
             foreach(char letter in word)
